d8532576b16d53eaacfb9289e3a7c3ebe611b53f
Anahit Margaryan <anahitmargaryan2221@gmail.com>
2021-07-09 10:17:43 +0400

    do onBoarding screen visible once

--- Practice/AppDelegate.swift
+++ Practice/AppDelegate.swift
@@ -9,13 +9,23 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    var window: UIWindow?
+    private let storageManager = StorageManager()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+       
+        storageManager.resetOnboardingSeen()
         return true
     }
+    
+//    private func configureAppLauching() {
+//        guard let navigation = window?.rootViewController as? UINavigationController else { return }
+//
+////        navigation.viewControllers [ ]
+//    }
 
     // MARK: UISceneSession Lifecycle
 
